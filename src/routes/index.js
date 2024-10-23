894685401588acb19c2b049fe9e4826d318901b6
Vinícius Scott <viniciusfscott@gmail.com>
2024-10-23 20:15:13 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,13 @@ router.get('/', (req, res) => {
   res.send({ message: 'Hello world' });
 });
 
+router.post('/users-list', (req, res) => {
+  const usersList = req.body;
+  console.log(usersList)
+
+  res.send({
+    message: 'New User was added to the list'
+  });
+});
+
 module.exports = router;
